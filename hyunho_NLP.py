69af72708510f730c57ee3384028648e28c4dfcf
Kiyong <gy8971@gmail.com>
2018-03-13 20:15:56 +0900

    Integrated sources

--- hyunho_NLP.py
+++ hyunho_NLP.py
@@ -1,14 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from gensim.models import Word2Vec
 from konlpy.corpus import kobill
 import dbConnection as db
 
-
-
 files_ko = kobill.fileids()
-
-
-#doc_ko = [ '26개 주사위로 이뤄진 정육면체' , '단 0.38초만에 맞추는 로봇이 나왔다.', '단 0.5초를 제외하고 로봇이뭘까' ]
-
 doc_ko = [] # array 선언
 
 try:
@@ -24,12 +20,44 @@ finally:
 
 
 
-from konlpy.tag import Twitter; t = Twitter()
+from konlpy.tag import Twitter;
+t = Twitter()
 
 noun_token = []
-for i in range(len(doc_ko)) :
-    tokens_ko = t.morphs( doc_ko[i] )
-    noun_token.append( tokens_ko )
+rep_noun = [] # rep is representation
+
+from collections import Counter
+for i in range( len( doc_ko ) ) :
+    tokens_ko = t.morphs(doc_ko[i])
+    noun_token.append(tokens_ko)
+
+    cnt_tokens_ko = t.nouns(doc_ko[i]) #의미단어 검출
+    tmp_arr = []
+    for j in range(len(cnt_tokens_ko)):
+        if len(cnt_tokens_ko[j]) >1:
+            tmp_arr.append(cnt_tokens_ko[j])
+            cnt = Counter(tmp_arr)
+    noun_token.append(tmp_arr)
+    rep_noun.append(cnt.most_common(5))
+
+
+'''
+
+try:
+    with db.connection.cursor() as cursor:
+        for i in range(len(rep_noun)):
+            value = rep_noun[i]
+            sql = 'INSERT INTO article_representation_noun(noun_1,noun_2,noun_3,noun_4,noun_5) VALUES (%s,%s,%s,%s,%s)'
+            cursor.execute(sql,(value[0][0],value[1][0],value[2][0],value[3][0],value[4][0]))
+    db.connection.commit()
+    print(cursor.lastrowid)
+    # 1 (last insert id)
+finally:
+    db.connection.close()
+
+'''
+
+
 
 
 print( "noun_token")
@@ -44,13 +72,15 @@ model = Word2Vec( noun_token , min_count = 1 , iter = 1000 )
 
 priority_arr = []
 
+
 for i in range(len(noun_token)):  # 문장개수
     for j in range(len(noun_token[i])):  # 해당문장의 단어수
         similarity = (int)(abs(model.wv.similarity('해마다', noun_token[i][j])) * 100)
         _a = t.pos(noun_token[i][j])
-        if _a[0][1] == "Noun" and len(_a[0][0]) > 1:
+        if _a[0][1] == "Noun" and _a[0][1] == "에서" and len(_a[0][0]) > 1:
             _tuple = noun_token[i][j], similarity
             priority_arr.append(_tuple)
+
 
 from operator import itemgetter
 
