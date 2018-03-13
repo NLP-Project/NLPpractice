f2899780ab33938a08385f6a24fc816006f962e0
Kiyong <gy8971@gmail.com>
2018-03-13 18:35:38 +0900

    Integrated sources

--- main.py
+++ main.py
@@ -82,3 +82,20 @@ print(priority_arr)
 
 
 
+
+
+
+def LoadModel() :
+    return Word2Vec.load('./word2vec.model')
+
+
+# model.save('./word2vec.model')
+# model = LoadModel()
+
+
+
+def article_similarity(article_1, article_2):
+    return (int)(abs(model.wv.similarity(article_1,article_1)) * 100)
+
+
+
