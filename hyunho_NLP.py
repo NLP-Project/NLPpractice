2347df8d1dba414828cefa5de0bc2cebe6a0af35
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-03-13 20:12:04 +0900

    Auto stash before merge of "master" and "origin/master"
    noun_token & model.wm.vocab confirm

---
+++ hyunho_NLP.py
@@ -0,0 +1,58 @@
+from gensim.models import Word2Vec
+from konlpy.corpus import kobill
+import dbConnection as db
+
+
+
+files_ko = kobill.fileids()
+
+
+#doc_ko = [ '26개 주사위로 이뤄진 정육면체' , '단 0.38초만에 맞추는 로봇이 나왔다.', '단 0.5초를 제외하고 로봇이뭘까' ]
+
+doc_ko = [] # array 선언
+
+try:
+    with db.connection.cursor() as cursor:
+        sql = "SELECT content FROM article"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+
+        for i in range(len(result)):
+            doc_ko.append(result[i][0])
+finally:
+    cursor.close()
+
+
+
+from konlpy.tag import Twitter; t = Twitter()
+
+noun_token = []
+for i in range(len(doc_ko)) :
+    tokens_ko = t.morphs( doc_ko[i] )
+    noun_token.append( tokens_ko )
+
+
+print( "noun_token")
+print(noun_token)
+print()
+
+
+
+print('*********************************')
+
+model = Word2Vec( noun_token , min_count = 1 , iter = 1000 )
+
+priority_arr = []
+
+for i in range(len(noun_token)):  # 문장개수
+    for j in range(len(noun_token[i])):  # 해당문장의 단어수
+        similarity = (int)(abs(model.wv.similarity('해마다', noun_token[i][j])) * 100)
+        _a = t.pos(noun_token[i][j])
+        if _a[0][1] == "Noun" and len(_a[0][0]) > 1:
+            _tuple = noun_token[i][j], similarity
+            priority_arr.append(_tuple)
+
+from operator import itemgetter
+
+priority_arr = sorted(priority_arr, key=itemgetter(1), reverse=True)  # sort by Second Value
+print(priority_arr)
