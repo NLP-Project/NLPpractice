3c65eced96adcb3ba7fc7ed88c9e0bf0f45c7138
Kiyong <gy8971@gmail.com>
2018-03-13 19:06:42 +0900

    Edit Code

--- main.py
+++ main.py
@@ -29,21 +29,21 @@ t = Twitter()
 
 noun_token = []
 
-rep_noun = []
+rep_noun = [] # rep is representation
 from collections import Counter
 
 for i in range( len( doc_ko ) ) :
     tokens_ko = t.nouns(doc_ko[i]) #의미단어 검출
-    # print(tokens_ko)
-    # print(len(tokens_ko[0]))
+    tmp_arr = []
     for j in range(len(tokens_ko)):
         if len(tokens_ko[j]) >1:
-            # print(tokens_ko[j])
-            noun_token.append(tokens_ko[j])
-            cnt = Counter(noun_token)
-
+            tmp_arr.append(tokens_ko[j])
+            cnt = Counter(tmp_arr)
+    noun_token.append(tmp_arr)
     rep_noun.append(cnt.most_common(5))
 
+
+'''
 
 try:
     with db.connection.cursor() as cursor:
@@ -57,18 +57,21 @@ try:
 finally:
     db.connection.close()
 
+'''
+
 
 print('*********************************')
-
+print(noun_token)
 model = Word2Vec(noun_token, iter=1000)
-
+print('*********************************')
+print(model.wv.vocab)
 priority_arr = []
 
-print(model.most_similar('맥아'))
+print(model.most_similar('거리'))
 for i in range(len(noun_token)):  # 문장개수
     for j in range(len(noun_token[i])):  # 해당문장의 단어수
         print(noun_token[i][j])
-        similarity = (int)(abs(model.wv.similarity('플랫폼', noun_token[i][j])) * 100)
+        similarity = (int)(abs(model.wv.similarity('거리', noun_token[i][j])) * 100)
         _a = t.pos(noun_token[i][j])
         if _a[0][1] == "Noun" and _a[0][1] == "에서" and len(_a[0][0]) > 1:
             _tuple = noun_token[i][j], similarity
